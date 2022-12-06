7d5fd6530babca5538f2ce3c2ac3a3dc5ddf6238
Ali Ghazal <ali.mdghazal@gmail.com>
2022-12-06 14:30:55 +0200

    exercise 3.1 - 3.2

---
+++ phonebook-backend/index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const app = express()
+
+let phonebook_entries = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.use(express.json())
+
+app.get('/api/persons', (req, res) => {
+    res.send(phonebook_entries)
+})
+
+app.get('/info', (req, res) => {
+    res.send( `<div>
+        <p> Phonebook has info for ${ phonebook_entries.length } people </p>
+        <p> ${Date().toString()}  </p>
+    </div>`  )
+})
+
+
+  
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
