b962e6f05659c36d6a7cfca5c12bc4b104d9382e
Ali Ghazal <ali.mdghazal@gmail.com>
2022-12-09 17:45:38 +0200

    exercise 3.10 - 3.11

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -102,9 +102,10 @@ app.post('/api/persons', (req, res) => {
     res.json(entry)
 })
 
+app.use(express.static('build'))
 
   
-const PORT = 3001
+const PORT = process.env.PORT || "8080";
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
