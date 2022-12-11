59072095397323ab9a0472805612fff2e44f17f3
Ali Ghazal <ali.mdghazal@gmail.com>
2022-12-12 01:52:26 +0200

    [exercise 3.19] schema valid & errs handling

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -13,35 +13,13 @@ app.use(express.json())
 
 
 morgan.token('body', (req, res) => {
-    if (req.method == 'POST')
-        return JSON.stringify( req.body )
-    else
-        return ""
+  if (req.method == 'POST')
+    return JSON.stringify(req.body)
+  else
+    return ""
 });
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let phonebook_entries = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
 
 app.use(express.json())
 
@@ -52,81 +30,83 @@ app.get('/api/persons', (req, res) => {
 })
 
 app.get('/api/persons/:id', (req, res) => {
-    const id = String(req.params.id)
+  const id = String(req.params.id)
 
-    Person.find({"_id": id}).then(result => {
-      return res.json(result)
-    })
+  Person.find({ "_id": id }).then(result => {
+    return res.json(result)
+  })
 
-    // res.status(404).send({ error: 'entry not found' })
+  // res.status(404).send({ error: 'entry not found' })
 })
 
 
 app.delete('/api/persons/:id', (req, res, next) => {
-    const id = String(req.params.id)
+  const id = String(req.params.id)
 
-    Person.findByIdAndRemove(id)
+  Person.findByIdAndRemove(id)
     .then(result => {
       res.status(204).end()
     })
     .catch(error => next(error))
-  })
+})
 
 app.get('/info', (req, res) => {
 
-    Person.count({}, function( err, count){
-      console.log( "Number of users:", count );
-      res.send( `<div>
-        <p> Phonebook has info for ${ count } people </p>
+  Person.count({}, function (err, count) {
+    console.log("Number of users:", count);
+    res.send(`<div>
+        <p> Phonebook has info for ${count} people </p>
         <p> ${Date().toString()}  </p>
       </div>`  )
-    })
+  })
 
 
-    
+
 })
 
-app.post('/api/persons/', (req, res)  =>  {
-    const body = req.body
-  
-    if (!body.name || !body.number ) {
-      return res.status(400).json({ 
-        error: 'The name or number is missing' 
-      })
-    }
-  
-    const person = new Person( {
-        number: body.number,
-        name: body.name,
-    })
+app.post('/api/persons/', (req, res, next) => {
+  const body = req.body
 
-    person.save().then( savedPerson => {
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: 'The name or number is missing'
+    })
+  }
+
+  const person = new Person({
+    number: body.number,
+    name: body.name,
+  })
+
+  person.save()
+    .then(savedPerson => {
       res.json(savedPerson)
-    } )
-    
+    })
+    .catch(error => next(error))
+
 })
 
-app.put('/api/persons/:id', (req, res, next)  =>  {
+app.put('/api/persons/:id', (req, res, next) => {
   const body = req.body
   const id = String(req.params.id)
 
-  if (!body.name || !body.number ) {
-    return res.status(400).json({ 
-      error: 'The name or number is missing' 
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: 'The name or number is missing'
     })
   }
 
   const person = {
-      number: body.number,
-      name: body.name,
+    number: body.number,
+    name: body.name,
   }
 
-  Person.findByIdAndUpdate(id, person, { new: true })
+  Person.findByIdAndUpdate(id, person,  { new: true, runValidators: true, context: 'query' })
     .then(updatedPerson => {
       res.json(updatedPerson)
     })
-  .catch(error => next(error))
-  
+    .catch(error => next(error))
+
 })
 
 app.use(express.static('build'))
@@ -134,12 +114,17 @@ app.use(express.static('build'))
 
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
+
+  if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
   next(error)
 }
 
 app.use(errorHandler)
 
-  
+
 const PORT = process.env.PORT || "8080";
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
